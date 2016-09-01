a9745108ec452041634c9856b1731e9cdc835066
Tiriah Kinsey <tkinseywalton001@student.butte.edu>
2016-09-01 07:22:45 +0000

    Lab2 submission

---
+++ lab2/lab2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+/*
+This is a simple number guessing game
+
+First
+
+Select random number between 1 and 20.
+Get user input. 
+If user guesses the number 14, the user wins and receives 10 points. 
+If user guesses below 14, tell the user "higher".
+If user guesses higher than 14, tell user "lower".
+For every wrong guess, take 1 point from 10 from user.
+
+End
+
+*/
+
+
