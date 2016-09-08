766a6b7ceeee9c0d3a541d73169388e60bcdfbd9
Tiriah Kinsey <tkinseywalton001@student.butte.edu>
2016-09-08 07:05:56 +0000

    Lab 3 Submission

--- lab3/lab3.cpp
+++ lab3/lab3.cpp
@@ -1,3 +1,54 @@
+// Created by: Tiriah Kinsey
+
 #include <iostream> 
 #include <string>
-#using namespace std; 
+using namespace std; 
+
+int main() {
+
+string nameMale = "";
+string nameFemale = "";
+
+char city;
+int numAgo;
+int numSince;
+float mileTime;
+
+/* 
+The Story of The Flash
+
+In Central City lives The Flash. His real name is Barry. 
+It was 14 years ago when his mom died. 
+Barry has been in love with his bestfriend Iris, since he was 11 years old. 
+At the speed of light, Flash can run a mile in 5.3 microseconds!
+*/
+
+cout << "Please enter a city." << endl;
+cin >> city;
+
+cout << "Please enter a male name." << endl;
+cin >> nameMale;
+
+cout << "Please enter an even number." << endl;
+cin >> numAgo;
+
+cout << "Please enter a female name." << endl;
+cin >> nameFemale;
+
+cout << "Please enter a whole number." << endl;
+cin >> numSince;
+
+cout << "Please a decimal number." << endl;
+cin >> mileTime;
+
+
+
+cout << "In " << city << " lives The Flash. " << endl;
+cout << "His real name is " << nameMale << "." << endl; 
+cout << "It was " << numAgo << " years ago when his mom died." << endl;
+cout << nameMale << " has been in love with his bestfriend " << nameFemale << ", since he was " << numSince << " years old." << endl;
+cout << "At the speed of light, Flash can run a mile in " << mileTime << " microseconds!" << endl;
+   
+return 0;
+
+}
