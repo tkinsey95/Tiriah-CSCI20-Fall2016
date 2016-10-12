64550fcc2f9cf6e508aa04ad27d8c27a13e6c46f
Tiriah Kinsey <tkinseywalton001@student.butte.edu>
2016-10-12 23:32:16 +0000

    Lab 4 submission

--- lab4/lab4.cpp
+++ lab4/lab4.cpp
@@ -9,9 +9,14 @@ int amtNickels = 0;
 int amtPennies = 0;
 int totalCents = 0;
 int totalAfterFee = 0;
+int shopping, fee;
 
 cout << "Enter your coins into the machine: ";
 cin >> totalCents;
+
+cout << "Are you shopping?" << endl; 
+cin >> shopping;
+
 
 
 amtQuarters = totalCents/25;
@@ -35,6 +40,13 @@ cout << "Amount of pennies: " << amtPennies << endl;
 cout << "Total amount of money: " << totalCents << endl;
 cout << "The total after the fee: " << totalAfterFee << endl;
 cout << "Total after fee: " << (totalCents*0.109) << totalAfterFee;
+
+if (shopping) {
+    fee = 0.05;
+}
+else {
+    fee = 0.10;
+}
 
 return 0;
 
