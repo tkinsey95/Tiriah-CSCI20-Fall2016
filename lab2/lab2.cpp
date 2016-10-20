1c7a7a655f98e51ca2df8aef8434bed72203a840
Tiriah Kinsey <tkinseywalton001@student.butte.edu>
2016-10-20 19:23:53 +0000

    Lab2 submission

--- lab2/lab2.cpp
+++ lab2/lab2.cpp
@@ -8,10 +8,11 @@ First
 
 Select random number between 1 and 20.
 Get user input. 
-If user guesses the number 14, the user wins and receives 10 points. 
-If user guesses below 14, tell the user "higher".
-If user guesses higher than 14, tell user "lower".
-For every wrong guess, take 1 point from 10 from user.
+If user guesses the selected number, the user wins and receives 10 points. 
+Else if user guesses the selected number, tell the user "higher".
+Else if user guesses higher than the selected number, tell user "lower".
+For every wrong guess, take 1 point from the 10 points the user has.
+Continue this until the user guesses the correct number or loses all of its points.
 
 End
 
