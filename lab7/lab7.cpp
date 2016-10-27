369b04e678fe2a681e3e91617328bd15a63a3fe8
Tiriah Kinsey <tkinseywalton001@student.butte.edu>
2016-10-27 19:45:27 +0000

    Lab7 submission

---
+++ lab7/lab7.cpp
@@ -0,0 +1,17 @@
+#include <iostream> 
+#include <string>
+using namespace std; 
+
+
+int main() {
+
+
+     string chorus = "Row row row your boat, gently down the stream.";
+     string verse1 = "Coding can be fun please try not to scream.";
+     string verse2 = "Merrily merrily merrily merrily, life is but a dream.";
+ 
+    cout << chorus << verse1 << chorus << verse2 << endl;
+
+}
+  
+ 
