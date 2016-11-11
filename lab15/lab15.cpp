b3886f07a5b9d92e2128c674b88f708864170c8e
Tiriah Kinsey <tkinseywalton001@student.butte.edu>
2016-11-11 04:59:57 +0000

    Lab15 submission

--- lab15/lab15.cpp
+++ lab15/lab15.cpp
@@ -1,25 +1,69 @@
 #include <iostream> 
 using namespace std;
 
+
+ void menuOptions() {
+            cout << "Enchiladas: 0" << endl;
+            cout << "Hamburgers: 1" << endl;
+            cout << "Burritos: 2" << endl;
+            cout << "Tacos: 3" << endl;
+            cout << "Salad: 4" << endl;
+            cout << "Cheesecake: 5" << endl;
+            cout << "Muffins: 6" << endl;
+            cout << "Pancakes: 7" << endl;
+            cout << "Hashbrowns: 8" << endl;
+            cout << "Pizza: 9" << endl;
+                    }
 int main () {
-    const int menu_Items = 10;
-    int itemAmnt [menu_Items];
+    int menuNum[10] = {0,0,0,0,0,0,0,0,0,0};
+    char userIn = '0';
+    string menu[10];
+    int userChoice = '0';
     int i = 0;
-    double useritemAmnt = 0;
+    char done;
+    done = 'd'; 'D';
     
-    itemAmnt[i] = 1;
-    itemAmnt[i] = 2;   
-    itemAmnt[i] = 3;  
-    itemAmnt[i] = 4;  
-    itemAmnt[i] = 5;  
-    itemAmnt[i] = 6;  
-    itemAmnt[i] = 7; 
-    itemAmnt[i] = 8;  
-    itemAmnt[i] = 9; 
-    itemAmnt[i] = 10;  
+    cout << "The items you can choose from: " << endl;
+    menuOptions();
+ 
+    menu[0] = "Enchiladas";
+    menu[1] = "Hamburgers";
+    menu[2] = "Burritos";
+    menu[3] = "Tacos";
+    menu[4] = "Salad";
+    menu[5] = "Cheesecake";
+    menu[6] = "Muffins";
+    menu[7] = "Pancakes";
+    menu[8] = "Hashbrowns";
+    menu[9] = "Pizza";
     
-    cout << "Enter the amount of items:  " << endl;
-    cin >> useritemAmnt;
+
     
-return 0;
+    while (userChoice != 'D' || userChoice != 'd') {
+        
+        
+        cout << "Choose type d or D to indicate that you are done choosing an item." << endl; 
+        cout << "To select an item off of the menu select a number:  " << endl;
+        cin >> userChoice;    
+      
+    }
+    
+   if (userChoice <= 9) {
+    
+        cout << userChoice << endl;
+        cin >> userChoice;
+    }
+    
+  
+     else if (userChoice > 10) {
+        cout << "I'm sorry, but you entered an invalid entry. Please enter a value found on the menu" << endl;   
+    }
+
+    
+    if ( userChoice == 'd' || userChoice == 'D') {
+    cout << "Goodbye." << endl;
+    cin >> done;
+   
+    
+    }
 }
