b01f30dbf4ce8cf238e80ada7eff9e8d14662ae2
Tiriah Kinsey <tkinseywalton001@student.butte.edu>
2016-11-24 05:19:07 +0000

    Lab15 submission

--- lab15/lab15.cpp
+++ lab15/lab15.cpp
@@ -1,20 +1,17 @@
 #include <iostream> 
+#include <vector>
 using namespace std;
 
 
- void menuOptions(string menu[]) {
+ void menuOptions(string menu[])
+ {
      for (int i=0; i <10; i++)
         cout << menu[i] << endl;
-       
  }
  
-int main () {
- 
-      
-   
+int main () 
+{
     int menuNum[10] = {0,0,0,0,0,0,0,0,0,0};
-    
-    
     char userIn = '0';
     string menu[10] = {"Enchiladas", "Hamburgers", "Burritos", "Tacos" "Salad", "Cheesecake", "Muffins", "Pancakes", "Hashbrowns", "Pizza"};
     char userChoice = 'x';
@@ -22,39 +19,38 @@ int main () {
     char done;
     done = 'd'; 'D';
     
-    cout << "The items you can choose from: " << endl;
-    menuOptions(menu);
+       cout << "The items you can choose from: " << endl;
+       menuOptions(menu);
  
-    
-    
     while (userChoice != 'D' && userChoice != 'd') {
-        
-        
+      
         cout << "Choose type d or D to indicate that you are done choosing an item." << endl; 
         cout << "To select an item off of the menu select a number:  " << endl;
-        cin >> userChoice;
+         cin >> userChoice;
       
+ std::vector <std::string> previousOrders;
     
    if (userChoice <= 9 + 48) {
     
         cout << "you chose: " << menu[userChoice - 48] << endl;
-    }
+        previousOrders.push_back(menu[userChoice]);
+         
+   }
+ for (int i = 0; i < previousOrders.size(); i++) 
+  {
+   std::cout << previousOrders[i] << std::endl;
+   
+  }
     
-     else if (userChoice > 10 + 48 && userChoice != 'd' && userChoice != 'D') {
-        cout << "I'm sorry, but you entered an invalid entry. Please enter a value found on the menu" << endl;   
-    }
-
-    
-    if ( userChoice == 'd' || userChoice == 'D') {
-   menuNum[userChoice - 48] = 1;
-
-    cout << "Goodbye." << endl;
-
-    
+ if ( userChoice == 'd' || userChoice == 'D') 
+    {
+     menuNum[userChoice - 48] = 1;
+     menuNum[userChoice - 48] = 2;
+       cout << "Goodbye." << endl;
     }    
     
    
     }
-cout << "you chose: " << menuNum[userChoice + 48] << endl;
+cout << "you chose: " << menuNum[userChoice - 48] << endl;
     
 }
